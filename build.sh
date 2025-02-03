c9fde098a22ae7fa1d791aec6ad33cbc7c601286
JuanNahual <enzolpz90@gmail.com>
2025-02-03 13:53:44 -0300

    variables de entorno

--- build.sh
+++ build.sh
@@ -7,3 +7,16 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+# Crear superusuario con variables de entorno
+echo "from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superusuario creado con éxito')
+else:
+    print('El superusuario ya existe')" | python manage.py shell
