493cd946c8c2c74fb892b40804c0fe806d30ace1
JuanNahual <enzolpz90@gmail.com>
2025-02-03 13:43:32 -0300

    agregando allow hots

--- bigdata/settings.py
+++ bigdata/settings.py
@@ -141,3 +141,4 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ALLOWED_HOSTS = ['biggdataa.onrender.com']
