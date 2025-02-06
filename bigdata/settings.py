f1a48bf2dbe0857a3212ada8debdb709b46ff270
JuanNahual <enzolpz90@gmail.com>
2025-02-06 15:16:13 -0300

    Update settings.py

--- bigdata/settings.py
+++ bigdata/settings.py
@@ -140,10 +140,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static',)
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
     # Agregar media a rutas permitidas en producción
